9c8c077bbf5a423f906f8fc02fac6ae4d2a34211
Fernando Lorenzo <108877985+fernandoalorenzo@users.noreply.github.com>
2025-07-22 21:51:32 -0300

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -5,7 +5,8 @@ import serumAntiedad from '@/assets/serum-antiedad.jpg';
 import labialesNatura from '@/assets/labiales-natura.jpg';
 
 export const products = [
-    "id": 1,
+ {
+  "id": 1,
   "name": "Perfumito para pies",
   "price": 152000,
   "image": "https:\/\/www.kine-estetic.com\/web-experto\/representations\/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBCRjBvZWdFPSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--ae0442c6d58f3bc93ed614813d2b8f8640da8c9c\/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9MY21WemFYcGxTU0lOTmpBd2VEWXdNRDRHT2daRlZEb01ZMjl1ZG1WeWREb0pkMlZpY0RvTWNYVmhiR2wwZVdrMyIsImV4cCI6bnVsbCwicHVyIjoidmFyaWF0aW9uIn19--2f62044b0fbadd16c42c4edfd24a89009f3cd914\/1751535413-17010637-1.jpg",
